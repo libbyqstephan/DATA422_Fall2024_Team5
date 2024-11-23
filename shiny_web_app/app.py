c7b8895bd4c65f2c6f38b3baad9f047221b6f33e
PrelaunchZeus2 <jfsalmoniv@gmail.com>
2024-11-22 16:10:05 -0800

    now with: TABS! yaay

--- shiny_web_app/app.py
+++ shiny_web_app/app.py
@@ -1,23 +1,40 @@
 from shiny import render, ui
-from shiny.express import input
+from shiny.express import input, ui
 import matplotlib.pyplot as plt
 import numpy as np
 
-ui.panel_title("I have No Idea What I am Doing")
-ui.input_slider("p", "Please Enter Phone Number", 1111111111, 9999999999, 0)
+with ui.navset_pill(id="tab"):  
+    with ui.nav_panel("Overview"):
+        "Panel A content"
 
-@render.code
-def txt():
-    s = str(input.p())
-    if input.p()**2 - 6741414522*input.p() + 1.06669e19 == 0:
-        return "lol"
-    else:
-        return f"phone number is ({s[0:3]}){s[3:6]}-{s[6:10]}"
+    with ui.nav_panel("Tabulations"):
+        "Panel B content"
 
-@render.plot
-def graph_thing():
-    fig, ax = plt.subplots()
-    ax.plot([1, 2, 3], [1, 2, 3])
-    plot = ax
-    return fig
+    with ui.nav_panel("Visuals & Charts"):
+        "Panel C content"
+        @render.plot
+        def graph_thing():
+            fig, ax = plt.subplots()
+            ax.plot([1, 2, 3], [1, 2, 3])
+            plot = ax
+            return fig
+
+
+    with ui.nav_panel("Models"):
+        "Page D content"
+        
+    with ui.nav_panel("test"):
+        ui.panel_title("I have No Idea What I am Doing")
+        ui.input_slider("p", "Please Enter Phone Number", 1111111111, 9999999999, 0)
+
+
+        @render.code
+        def txt():
+            s = str(input.p())
+            if input.p()**2 - 6741414522*input.p() + 1.06669e19 == 0:
+                return "lol you found me"
+            else:
+                return f"phone number is ({s[0:3]}){s[3:6]}-{s[6:10]}"
+
+
 
